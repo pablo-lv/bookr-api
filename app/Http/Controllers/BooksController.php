137e3a9e232d99e69234e55115d703bae0f08d32
Pablo Lucas <pablo.lucv@gmail.com>
2016-05-30 18:21:54 -0500

    Delete a Book

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -78,5 +78,26 @@ class BooksController extends Controller
         return $book;
     }
 
+    /**
+     * DELETE /books/{id}
+     * @param $id
+     * @return \Laravel\Lumen\Http\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $book = Book::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'error' => [
+                    'message' => 'Book not found'
+                ]
+            ], 404);
+        }
+        $book->delete();
+
+        return response(null, 204);
+    }
+
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,3 +22,4 @@ $app->get('/books/{id:[\d]+}', [
 ]);
 $app->post('/books', 'BooksController@store');
 $app->put('/books/{id:[\d]+}', 'BooksController@update');
+$app->delete('/books/{id:[\d]+}', 'BooksController@destroy');
